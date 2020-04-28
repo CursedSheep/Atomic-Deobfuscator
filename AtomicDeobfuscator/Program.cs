e8c5f67eaffdd467d728f893bd1b702024d9c371
CursedSheep <44673993+CursedSheep@users.noreply.github.com>
2020-04-28 21:15:11 +0800

    Add files via upload

---
+++ AtomicDeobfuscator/Program.cs
@@ -0,0 +1,38 @@
+﻿using dnlib.DotNet;
+using dnlib.DotNet.Writer;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtomicDeobfuscator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename;
+            try
+            {
+                filename = args[0];
+            }
+            catch
+            {
+                Console.Write("Enter path: ");
+                filename = Console.ReadLine().Replace(@"""", "");
+            }
+            Console.Title = "Atomic Deobfuscator By CursedSheep ;D";
+            Console.WriteLine("Atomic Deobfuscator by Cursedsheep ;D");
+            ModuleDefMD module = ModuleDefMD.Load(filename);
+            Console.WriteLine("Hide methods: " + hideMethodsRemover.Deobfuscate(module));
+            ModuleWriterOptions mod = new ModuleWriterOptions(module);
+            mod.MetadataLogger = DummyLogger.NoThrowInstance;
+            string patho = Path.GetDirectoryName(filename) + "\\" + Path.GetFileNameWithoutExtension(filename) + "-Deobfuscated" + Path.GetExtension(filename);
+            module.Write(patho, mod); 
+            Console.WriteLine("saved to " + patho);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ AtomicDeobfuscator/hideMethodsRemover.cs
@@ -0,0 +1,59 @@
+﻿using dnlib.DotNet;
+using dnlib.DotNet.Emit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtomicDeobfuscator
+{
+    class hideMethodsRemover
+    {
+        private static OpCode[] list = { OpCodes.Call, OpCodes.Sizeof, OpCodes.Calli };
+        private static bool detect(ModuleDefMD module)
+        {
+            foreach (TypeDef type in module.GetTypes())
+            {
+                foreach (MethodDef method in type.Methods)
+                {
+                    if (!method.HasBody || !method.Body.HasInstructions || !method.Body.HasExceptionHandlers) continue;
+                    var exceptionhandlers = method.Body.ExceptionHandlers.ToArray();
+                    foreach (ExceptionHandler fd in exceptionhandlers)
+                    {
+                        if (list.Contains(fd.TryStart.OpCode) && fd.TryStart.Operand == null) return true;
+                    }
+                }
+            }
+            return false;
+        }
+        public static int Deobfuscate(ModuleDefMD module)
+        {
+            bool hasprotection = detect(module);
+            int counter = 0;
+            if (!hasprotection) return counter;
+            foreach (TypeDef type in module.GetTypes())
+            {
+                foreach (MethodDef method in type.Methods)
+                {
+                    if (!method.HasBody || !method.Body.HasInstructions || !method.Body.HasExceptionHandlers) continue;
+                    var exceptionhandlers = method.Body.ExceptionHandlers.ToArray();
+                    foreach (ExceptionHandler fd in exceptionhandlers)
+                    {
+                        if (list.Contains(fd.TryStart.OpCode) && fd.TryStart.Operand == null)
+                        {
+                            var instructions = method.Body.Instructions;
+                            //int startIndex = method.Body.Instructions.IndexOf(fd.HandlerStart);
+                            int endIndex = instructions.IndexOf(fd.TryEnd);
+                            for (int i = 0; i < endIndex; i++) instructions[i].OpCode = OpCodes.Nop;
+                            method.Body.ExceptionHandlers.Remove(fd);
+                            counter++;
+
+                        }
+                    }
+                }
+            }
+            return counter;
+        }
+    }
+}
